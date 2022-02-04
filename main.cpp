ece486e57647a89dadd81e4ce4887a42befb8f0b
E1Hephaestus <gefestroyale@gmail.com>
2022-02-04 17:22:45 +0200

    Created window

--- main.cpp
+++ main.cpp
@@ -3,5 +3,13 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "sdf");
+
+    while(window.isOpen()){
+        window.clear();
+
+        window.display();
+    }
+
     return 0;
 }
